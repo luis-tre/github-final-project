ffc36a6e905b454d11df05b2a5f43883f5a9176e
luis-tre <93397117+luis-tre@users.noreply.github.com>
2023-08-02 03:41:03 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <Luis-tre>
    # Input:
    # p, principal amount
    # t, time period in years
